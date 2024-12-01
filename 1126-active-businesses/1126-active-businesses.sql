585b3d8a1dd44a993132bb6a5713c6bbf1d453e5
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-12-01 00:24:05 -0800

    Update 1126-active-businesses.sql

--- 1126-active-businesses/1126-active-businesses.sql
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -10,3 +10,24 @@ ON a.event_type = b.event_type
 WHERE a.occurences > b.av
 GROUP BY business_id
 HAVING COUNT(*) > 1;
+
+
+
+# average activity : average occurences across all companies that have this event.
+# Active Business : more than one event_type
+# and occurences is strictly greater than the average activity for that event.
+
+with Events_CTE as (
+        select 
+            business_id,
+            event_type, 
+            occurrences,
+            avg(occurrences) over ( partition by event_type) as avg_activity
+        from Events )
+select
+     business_id
+from Events_CTE
+where occurrences > avg_activity
+group by business_id
+having count(distinct event_type) > 1
+
