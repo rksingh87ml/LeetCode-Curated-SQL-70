310353f20a163d4b017474cc1ed28fc499c3c310
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-11-30 16:16:23 -0800

    Update 0603-consecutive-available-seats.sql

--- 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
+++ 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
@@ -1,8 +1,23 @@
 # Write your MySQL query statement below
-SELECT DISTINCT a.seat_id
-FROM cinema a
-JOIN cinema b
-    ON ABS(a.seat_id - b.seat_id) = 1
-    AND a.free = 1
-    AND b.free = 1
-ORDER BY a.seat_id;
+# 1) report all the consecutive available seats in the cinema, so pick only free = 1
+# 2)  ordered by seat_id in ascending order.
+# 3) The test cases are generated so that more than two seats are consecutively available.
+
+# need to check prev and next seat using lag and lead functions
+with available_seat as 
+                    (
+                        select * from Cinema
+                        where free = 1
+
+                    ),
+    Cinema_CTE as (
+            select 
+            seat_id ,
+            seat_id - lag(seat_id,1) over ( order by seat_id asc ) as prev_seat,
+            seat_id - lead(seat_id,1) over (order by seat_id asc) as next_seat
+            from available_seat 
+            )
+select seat_id
+ from Cinema_CTE
+where prev_seat = 1 or next_seat = -1
+order by seat_id
