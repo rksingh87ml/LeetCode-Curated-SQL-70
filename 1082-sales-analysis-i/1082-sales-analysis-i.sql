f3e2fe2c5d11e005caf27f054480faf6073a87d3
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-11-30 21:27:00 -0800

    Update 1082-sales-analysis-i.sql

--- 1082-sales-analysis-i/1082-sales-analysis-i.sql
+++ 1082-sales-analysis-i/1082-sales-analysis-i.sql
@@ -1,8 +1,10 @@
-SELECT seller_id
-FROM Sales
-GROUP BY seller_id
-HAVING SUM(PRICE) >= ALL (
-    SELECT SUM(PRICE)
+SELECT 
+     seller_id
+FROM 
+    (SELECT 
+        seller_id,
+        DENSE_RANK() OVER (order by sum(price) desc ) as price_rank
     FROM Sales
-    GROUP BY seller_id
-)
+    GROUP BY seller_id) TEMP
+WHERE price_rank = 1
+
