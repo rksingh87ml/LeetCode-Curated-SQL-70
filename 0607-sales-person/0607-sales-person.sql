490c756604ef8b6a9f3f5fea7d9cc84ada8575a5
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-11-30 16:28:57 -0800

    Update 0607-sales-person.sql

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,12 +1,13 @@
 # Write your MySQL query statement below
-SELECT name
-FROM salesperson
-WHERE sales_id NOT IN (
-    SELECT sales_id
-    FROM orders
-    WHERE com_id IN (
-        SELECT com_id
-        FROM company
-        WHERE name = 'RED'
-    )
-)
+with ord_cte as (
+        select 
+            ord.order_id , ord.com_id , ord.sales_id , comp.name 
+        from Orders ord  join 
+        Company comp on comp.com_id = ord.com_id 
+        and comp.name = 'RED')
+select 
+    SP.name
+from SalesPerson SP
+left join ord_cte ord1
+on ord1.sales_id = SP.sales_id 
+where ord1.name is null
