5aaae456eadd8a3584c7eeb9e22977cfef782b1b
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-12-01 13:18:42 -0800

    Update 1164-product-price-at-a-given-date.sql

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -14,3 +14,20 @@ SELECT DISTINCT product_id, 10
 FROM Products
 GROUP BY product_id
 HAVING MIN(change_date) > '2019-08-16';
+
+
+
+with Products_CTE as (
+            select 
+            product_id,
+            new_price,
+            dense_rank() over ( partition by product_id order by change_date desc) as latest_price 
+            from Products
+            where change_date <= '2019-08-16')
+
+select 
+distinct PC1.product_id , coalesce(PC2.new_price,10) as price
+from Products PC1
+left join ( select * from Products_CTE where latest_price = 1) PC2
+on PC2.product_id = PC1.product_id
+
