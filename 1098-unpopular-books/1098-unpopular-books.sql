cc30b753fdf601398dff8267f351b6804538fada
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-11-30 22:12:31 -0800

    Update 1098-unpopular-books.sql

--- 1098-unpopular-books/1098-unpopular-books.sql
+++ 1098-unpopular-books/1098-unpopular-books.sql
@@ -1,23 +1,14 @@
 # Write your MySQL query statement below
-SELECT 
-    B.book_id,
-    B.name
-FROM
-    Books B
-LEFT JOIN
-    (SELECT 
-         O.book_id,
-         SUM(O.quantity) AS total_sales
-     FROM 
-         Orders O
-     WHERE 
-         O.dispatch_date BETWEEN DATE('2018-06-23') AND DATE('2019-06-23')
-     GROUP BY 
-         O.book_id
-    ) AS Sales
-ON 
-    B.book_id = Sales.book_id
-WHERE 
-    (Sales.total_sales < 10 OR Sales.total_sales IS NULL)
-AND 
-    B.available_from <= DATE('2019-05-23')
+select temp1.book_id, temp1.name
+from
+(select * 
+from Books 
+where available_from < date_sub('2019-06-23', INTERVAL 1 month)) temp1
+left join 
+(
+select *
+from Orders 
+where dispatch_date >= date_sub('2019-06-23', INTERVAL 1 YEAR)) temp2
+on temp2.book_id = temp1.book_id 
+group by temp1.book_id, temp1.name
+having coalesce(sum(quantity),0) < 10 
