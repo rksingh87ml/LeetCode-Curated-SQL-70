95fa8495f9f6d5007739dc2bbc1feb2cf4612c4c
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-12-02 09:58:52 -0800

    Update 1204-last-person-to-fit-in-the-bus.sql

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,9 +1,15 @@
-SELECT person_name
-FROM (
-    SELECT person_name, weight, turn, SUM(weight) OVER (ORDER BY turn) AS cum_sum
-    FROM queue
-) x
-WHERE cum_sum <= 1000
-ORDER BY turn DESC
-LIMIT 1;
+with CTE as (
+        select 
+            person_id,
+            person_name,
+            weight ,
+            sum(weight) over (order by turn asc) as running_totalweight
+        from Queue)
 
+select person_name 
+from (
+select 
+* , row_number() over ( order by running_totalweight desc) as rnk
+from CTE 
+where running_totalweight <=1000) temp
+where rnk = 1
