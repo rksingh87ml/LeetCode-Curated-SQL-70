a615a0f6ff6191700fff6561e331aec1ac116d10
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-12-02 09:26:15 -0800

    Update 1173-immediate-food-delivery-i.sql

--- 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
+++ 1173-immediate-food-delivery-i/1173-immediate-food-delivery-i.sql
@@ -1,3 +1,12 @@
 # Write your MySQL query statement below
 SELECT ROUND((100.0 * COUNT(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END)) / COUNT(*), 2) AS immediate_percentage
 FROM Delivery;
+
+
+
+select 
+    round(coalesce(count(delivery_id)*100 / temp.total_delievery,0),2) as immediate_percentage
+from Delivery join 
+      (select count(delivery_id) as total_delievery  from Delivery) as temp
+where order_date = customer_pref_delivery_date
+
