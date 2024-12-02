306cb024e8dd9d90d734522b7426f4a36e74698f
Ranjit Singh <72890793+rksingh87ml@users.noreply.github.com>
2024-12-02 14:37:26 -0800

    Update 1321-restaurant-growth.sql

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,14 +1,26 @@
 -- This query calculates the distinct visited dates, total amount for a 7-day rolling window, and average amount per day for each date.
 
-WITH cte AS (
-    SELECT visited_on,
-           SUM(amount) OVER (ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) AS amount,
-           DENSE_RANK() OVER (ORDER BY visited_on) AS rn
-    FROM Customer
-)
+with Customer_CTE as (
+        select 
+            visited_on, 
+            sum(amount) as total_amount
+        from Customer 
+        group by visited_on
+        order by visited_on),
+moving_average_CTE as 
+(
+select 
+    visited_on , 
+    total_amount,
+    sum(total_amount) over( order by visited_on rows between 6 preceding and current row )  as seven_days_amount,
+    row_number() over (order by visited_on) as rnk
+from Customer_CTE)
 
-SELECT DISTINCT visited_on,
-       amount,
-       ROUND(amount/7, 2) AS average_amount
-FROM cte
-WHERE rn >= 7;
+select 
+    visited_on , 
+    seven_days_amount as amount,
+    round(seven_days_amount/7 ,2) as average_amount
+from moving_average_CTE
+where rnk > 6
+order by visited_on
+
